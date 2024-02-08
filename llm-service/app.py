734622640a3c686c9484563c5f3a395a9bacd50c
JangSeHun <westreed@naver.com>
2024-02-08 18:34:45 +0900

    feat: Python으로 Provider 구현

--- llm-service/app.py
+++ llm-service/app.py
@@ -1,6 +1,7 @@
-import requests
 from fastapi import FastAPI
 from py_eureka_client import eureka_client
+
+from feign_provider import FeignProvider
 
 eureka_server = "http://discovery-service:8761/eureka"
 instance_port = 40010
@@ -8,29 +9,27 @@ instance_port = 40010
 app = FastAPI()
 
 
-@app.get('/info')
-async def test():
-    service_instances = await eureka_client.get_application(eureka_server=eureka_server, app_name="user-service")
-    if service_instances.instances:
-        service = service_instances.instances[-1]
-        target_url = f"http://{service.ipAddr}:{service.port.port}"
+@app.get('/info/{test_value}')
+async def info(test_value):
+    user_service = FeignProvider(eureka_server=eureka_server, app_name="user-service")
+    response = await user_service.a_get(f"/test/{test_value}")
 
-        # 예제로 GET 요청 보내기
-        response = requests.get(f"{target_url}/info")
-
-        # 응답 확인
-        if response.status_code == 200:
-            return f"요청 성공: {target_url} {response.text}"
-        else:
-            return f"요청 실패: {response.status_code} {response.text}"
+    # 응답 확인
+    if response.status_code == 200:
+        return f"요청 성공: user-service {response.text}"
     else:
-        print("서비스를 찾을 수 없습니다.")
+        return f"요청 실패: {response.status_code} {response.text}"
+
+
+@app.get('/test')
+async def test():
+    return f"요청 성공: llm-service /python/test"
 
 
 if __name__ == "__main__":
     import uvicorn
     eureka_client.init(
-        eureka_server="http://discovery-service:8761/eureka",  # "http://localhost:8761/eureka",
+        eureka_server=eureka_server,  # "http://localhost:8761/eureka",
         app_name="llm-service",
         # instance_host="127.0.0.1",
         instance_port=instance_port,
---
+++ llm-service/feign_provider.py
@@ -0,0 +1,83 @@
+import logging
+from enum import Enum
+
+import requests
+from py_eureka_client import eureka_client
+
+g_redis_scheduling_map = {}
+
+
+class StrEnum(str, Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Scheduling(StrEnum):
+    ROUND_ROBIN = 1
+
+
+class FeignProvider:
+
+    def __init__(self, eureka_server, app_name, scheduling="round-robin"):
+        self.eureka_server = eureka_server
+        self.app_name = app_name
+        self.scheduling = scheduling.upper().replace("-", "_")
+
+        global g_redis_scheduling_map
+        if not g_redis_scheduling_map.get(app_name):
+            g_redis_scheduling_map[app_name] = 0
+
+    def __scheduling(self, size):
+        global g_redis_scheduling_map
+        cur_index = g_redis_scheduling_map[self.app_name]
+
+        if self.scheduling == Scheduling.ROUND_ROBIN:
+            if size-1 > cur_index:
+                cur_index += 1
+            else:
+                cur_index = 0
+            g_redis_scheduling_map[self.app_name] = cur_index
+
+        return cur_index
+
+    async def __get_instances(self, index=-1):
+        service_instances = await eureka_client.get_application(eureka_server=self.eureka_server, app_name=self.app_name)
+        if len(service_instances.instances) <= index:
+            raise IndexError("존재하지 않는 서버 인덱스입니다.")
+        if index == -1:
+            index = self.__scheduling(len(service_instances.instances))
+        return service_instances.instances[index]
+
+    @staticmethod
+    def __create_target_url(instance, path):
+        if path and path[0] == "/":
+            i = 1
+            while i < len(path) and path[i] == "/":
+                i += 1
+
+            path = path[i:]
+        return f"http://{instance.ipAddr}:{instance.port.port}/{path}"
+
+    async def a_get(self, path, index=-1):
+        instance = await self.__get_instances(index)
+        target_url = self.__create_target_url(instance, path)
+        try:
+            response = requests.get(target_url)
+            return response
+        except Exception as e:
+            logging.error(f"GET {self.app_name}/{path} URI로 요청을 보내는 데 실패했습니다. {e}")
+
+    async def a_post(self, path, index=0, **kwargs):
+        instance = await self.__get_instances(index)
+        target_url = self.__create_target_url(instance, path)
+        try:
+            response = requests.post(target_url, json=kwargs)
+            return response
+        except Exception as e:
+            logging.error(f"POST {self.app_name}/{path} URI로 요청을 보내는 데 실패했습니다. {e}")
