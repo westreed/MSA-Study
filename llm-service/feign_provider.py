a34985f325c0faeef5dbb17518570537ef39b364
JangSeHun <westreed@naver.com>
2024-02-08 18:40:30 +0900

    fix: 용어 수정

--- llm-service/feign_provider.py
+++ llm-service/feign_provider.py
@@ -19,12 +19,12 @@ class StrEnum(str, Enum):
 
 
 class Scheduling(StrEnum):
-    ROUND_ROBIN = 1
+    SEQUENCE = 1
 
 
 class FeignProvider:
 
-    def __init__(self, eureka_server, app_name, scheduling="round-robin"):
+    def __init__(self, eureka_server, app_name, scheduling="sequence"):
         self.eureka_server = eureka_server
         self.app_name = app_name
         self.scheduling = scheduling.upper().replace("-", "_")
@@ -37,7 +37,7 @@ class FeignProvider:
         global g_redis_scheduling_map
         cur_index = g_redis_scheduling_map[self.app_name]
 
-        if self.scheduling == Scheduling.ROUND_ROBIN:
+        if self.scheduling == Scheduling.SEQUENCE:
             if size-1 > cur_index:
                 cur_index += 1
             else:
